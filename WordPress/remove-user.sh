4266cf313153cf8bfacb5fd5712c836f316eb5e5
SergioMeneses <sergioandresmeneses@gmail.com>
2018-04-04 21:35:19 -0500

    Remove users based on email/roll updated

--- WordPress/remove-user.sh
+++ WordPress/remove-user.sh
@@ -1,10 +1,14 @@
 #/bin/bash
+
+#We get a list of all the users with roll "subscriber" and filter them by the domain of the email.
 IDs=`wp user list --role="subscriber" | awk '{print $4}' | grep @something.com`
-echo "$IDs"
+
+#Print the total number of the users we found.
 n=`echo "$IDs" | wc -l`
 echo "subscribers: $n"
 echo " "
 
+#Run a 'FOR' for removing them.
 for i in $IDs
 do 
     wp user delete $i --reassign=no
